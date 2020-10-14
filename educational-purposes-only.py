41d2bc5b68148b38e8ccada37203236abcac4ba0
@cnagy <3061769+c-nagy@users.noreply.github.com>
2020-10-14 20:18:17 +0000

    Adding 1 secound delay during mon0 shutoff

--- educational-purposes-only.py
+++ educational-purposes-only.py
@@ -19,7 +19,7 @@ class EducationalPurposesOnly(plugins.Plugin):
         # Send command to Bettercap to stop using mon0:
         requests.post('http://127.0.0.1:8081/api/session', data='{"cmd":"wifi.recon off"}', auth=('pwnagotchi', 'pwnagotchi'))
         # Disable monitor mode interface mon0:
-        os.popen('ifconfig mon0 down && iw dev mon0 del')
+        os.popen('ifconfig mon0 down && sleep 1 && iw dev mon0 del')
         # Set wlan0 channel
         # Update wpa_supplicant.conf file
         # Start wpa_supplicant service:
